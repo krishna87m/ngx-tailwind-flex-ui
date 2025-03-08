80fbbba4161432018160c883ff01f400b7ab5ea8
krishna <krishna87m@gmail.com>
2025-03-08 15:41:33 +0530

    initialize a progress bar component

---
+++ libs/ngx-tailwind-flex-ui/src/lib/progress-bar/progress-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-progress-bar',
+  imports: [CommonModule],
+  templateUrl: './progress-bar.component.html',
+  styleUrl: './progress-bar.component.css',
+})
+export class ProgressBarComponent {}
